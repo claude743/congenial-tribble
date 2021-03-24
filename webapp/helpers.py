9b502429417e7009c68afb7738627dafd7c2d4f9
Mozartis74 <75519860+claude743@users.noreply.github.com>
2021-03-24 03:51:50 -0400

    Initialize snap_builds

---
+++ webapp/helpers.py
@@ -0,0 +1,104 @@
+import json
+
+import flask
+import os
+
+from ruamel.yaml import YAML
+
+
+_yaml = YAML(typ="rt")
+_yaml_safe = YAML(typ="safe")
+
+
+def get_yaml_loader(typ="safe"):
+    if typ == "safe":
+        return _yaml_safe
+    return _yaml
+
+
+def get_licenses():
+    try:
+        with open("webapp/store-licenses.json") as f:
+            licenses = json.load(f)
+
+        def _build_custom_license(license_id, license_name):
+            return {"licenseId": license_id, "name": license_name}
+
+        CUSTOM_LICENSES = [
+            _build_custom_license("Proprietary", "Proprietary"),
+            _build_custom_license("Other Open Source", "Other Open Source"),
+            _build_custom_license(
+                "AGPL-3.0+", "GNU Affero General Public License v3.0 or later"
+            ),
+            _build_custom_license(
+                "GPL-2.0+", "GNU General Public License v2.0 or later"
+            ),
+            _build_custom_license(
+                "GPL-3.0+", "GNU General Public License v3.0 or later"
+            ),
+            _build_custom_license(
+                "LGPL-2.1+", "GNU Lesser General Public License v2.1 or later"
+            ),
+            _build_custom_license(
+                "LGPL-3.0+", "GNU Lesser General Public License v3.0 or later"
+            ),
+        ]
+
+        licenses = licenses + CUSTOM_LICENSES
+    except Exception:
+        licenses = []
+
+    return licenses
+
+
+def get_default_track(snap_name):
+    # until default tracks are supported by the API we special case node
+    # to use 10, rather then latest
+    default_track = "10" if snap_name == "node" else None
+
+    return default_track
+
+
+def get_file(filename, replaces={}):
+    """
+    Reads a file, replaces occurences of all the keys in `replaces` with
+    the correspondant values and returns the resulting string or None
+
+    Keyword arguments:
+    filename -- name if the file to load.
+    replaces -- key/values to replace in the file content (default {})
+    """
+    filepath = os.path.join(flask.current_app.root_path, filename)
+
+    try:
+        with open(filepath, "r") as f:
+            data = f.read()
+            for key in replaces:
+                data = data.replace(key, replaces[key])
+    except Exception:
+        data = None
+
+    return data
+
+
+def get_yaml(filename, typ="safe", replaces={}):
+    """
+    Reads a file, replaces occurences of all the keys in `replaces` with the
+    correspondant values and returns an ordered dict with the YAML content
+
+    Keyword arguments:
+    filename -- name if the file to load.
+    typ -- type of yaml loader
+    replaces -- key/values to replace in the file content (default {})
+    """
+    try:
+        yaml = get_yaml_loader(typ)
+        data = get_file(filename, replaces)
+        return yaml.load(data)
+    except Exception:
+        return None
+
+
+def dump_yaml(data, stream, typ="safe"):
+    yaml = get_yaml_loader(typ)
+    yaml.dump(data, stream)
---
+++ webapp/publisher/views.py
@@ -0,0 +1,183 @@
+import flask
+import webapp.api.dashboard as api
+import webapp.api.marketo as marketo_api
+from webapp import authentication
+from webapp.api.exceptions import (
+    AgreementNotSigned,
+    ApiCircuitBreaker,
+    ApiError,
+    ApiResponseError,
+    ApiResponseErrorList,
+    ApiTimeoutError,
+    MacaroonRefreshRequired,
+    MissingUsername,
+)
+from webapp.decorators import login_required
+
+account = flask.Blueprint(
+    "account", __name__, template_folder="/templates", static_folder="/static"
+)
+
+marketo = marketo_api.Marketo()
+
+
+def refresh_redirect(path):
+    try:
+        macaroon_discharge = authentication.get_refreshed_discharge(
+            flask.session["macaroon_discharge"]
+        )
+    except ApiResponseError as api_response_error:
+        if api_response_error.status_code == 401:
+            return flask.redirect(flask.url_for("login.logout"))
+        else:
+            return flask.abort(502, str(api_response_error))
+    except ApiError as api_error:
+        return flask.abort(502, str(api_error))
+
+    flask.session["macaroon_discharge"] = macaroon_discharge
+    return flask.redirect(path)
+
+
+def _handle_error(api_error: ApiError):
+    if type(api_error) is ApiTimeoutError:
+        return flask.abort(504, str(api_error))
+    elif type(api_error) is MissingUsername:
+        return flask.redirect(flask.url_for(".get_account_name"))
+    elif type(api_error) is AgreementNotSigned:
+        return flask.redirect(flask.url_for(".get_agreement"))
+    elif type(api_error) is MacaroonRefreshRequired:
+        return refresh_redirect(flask.request.path)
+    elif type(api_error) is ApiCircuitBreaker:
+        return flask.abort(503)
+    else:
+        return flask.abort(502, str(api_error))
+
+
+def _handle_error_list(errors):
+    codes = [error["code"] for error in errors]
+
+    error_messages = ", ".join(codes)
+    return flask.abort(502, error_messages)
+
+
+@account.route("/")
+@login_required
+def get_account():
+    return flask.redirect(flask.url_for("publisher_snaps.get_account_snaps"))
+
+
+@account.route("/details", methods=["GET"])
+@login_required
+def get_account_details():
+    try:
+        # We don't use the data from this endpoint.
+        # It is mostly used to make sure the user has signed
+        # the terms and conditions.
+        api.get_account(flask.session)
+    except ApiResponseErrorList as api_response_error_list:
+        return _handle_error_list(api_response_error_list.errors)
+    except ApiError as api_error:
+        return _handle_error(api_error)
+
+    flask_user = flask.session["openid"]
+
+    subscriptions = None
+
+    # don't rely on marketo to show the page,
+    # if anything fails, just continue and don't show
+    # this section
+    try:
+        subscribed_to_newsletter = False
+        marketo_user = marketo.get_user(flask_user["email"])
+        if marketo_user:
+            marketo_subscribed = marketo.get_newsletter_subscription(
+                marketo_user["id"]
+            )
+            if marketo_subscribed.get("snapcraftnewsletter"):
+                subscribed_to_newsletter = True
+
+        subscriptions = {"newsletter": subscribed_to_newsletter}
+    except Exception:
+        if "sentry" in flask.current_app.extensions:
+            flask.current_app.extensions["sentry"].captureException()
+
+    context = {
+        "image": flask_user["image"],
+        "username": flask_user["nickname"],
+        "displayname": flask_user["fullname"],
+        "email": flask_user["email"],
+        "subscriptions": subscriptions,
+    }
+
+    return flask.render_template("publisher/account-details.html", **context)
+
+
+@account.route("/details", methods=["POST"])
+@login_required
+def post_account_details():
+    try:
+        newsletter_status = flask.request.form.get("newsletter")
+        email = flask.request.form.get("email")
+        marketo.set_newsletter_subscription(email, newsletter_status)
+        flask.flash("Changes applied successfully.", "positive")
+    except Exception:
+        flask.flash("There was an error, please try again.", "negative")
+
+    return flask.redirect(flask.url_for("account.get_account_details"))
+
+
+@account.route("/agreement")
+@login_required
+def get_agreement():
+    return flask.render_template(
+        "publisher/developer_programme_agreement.html"
+    )
+
+
+@account.route("/agreement", methods=["POST"])
+@login_required
+def post_agreement():
+    agreed = flask.request.form.get("i_agree")
+    if agreed == "on":
+        try:
+            api.post_agreement(flask.session, True)
+        except ApiResponseErrorList as api_response_error_list:
+            codes = [error["code"] for error in api_response_error_list.errors]
+            error_messages = ", ".join(codes)
+            flask.abort(502, error_messages)
+        except ApiError as api_error:
+            return _handle_error(api_error)
+
+        return flask.redirect(flask.url_for(".get_account"))
+    else:
+        return flask.redirect(flask.url_for(".get_agreement"))
+
+
+@account.route("/username")
+@login_required
+def get_account_name():
+    return flask.render_template("publisher/username.html")
+
+
+@account.route("/username", methods=["POST"])
+@login_required
+def post_account_name():
+    username = flask.request.form.get("username")
+
+    if username:
+        errors = []
+        try:
+            api.post_username(flask.session, username)
+        except ApiResponseErrorList as api_response_error_list:
+            errors = errors + api_response_error_list.errors
+        except ApiError as api_error:
+            return _handle_error(api_error)
+
+        if errors:
+            return flask.render_template(
+                "publisher/username.html", username=username, error_list=errors
+            )
+
+        return flask.redirect(flask.url_for(".get_account"))
+    else:
+        return flask.redirect(flask.url_for(".get_account_name"))
