3e79b5ca62d9fa3e037c6bb0b3e1cb82b0a30783
Mozartis74 <75519860+claude743@users.noreply.github.com>
2021-03-24 03:52:04 -0400

    Initialize increase-timeout

---
+++ webpack.config.entry.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "global-nav": "./static/js/base/global-nav.js",
+  base: "./static/js/base/base.js",
+  release: "./static/js/publisher/release.js",
+  public: "./static/js/public/public.js",
+  about: "./static/js/public/about/index.js",
+  tabpanel: "./static/js/public/hero-tabpanel.js",
+  // TODO:
+  // publisher bundle is big (webpack warning) - try to chunk it down
+  // https://github.com/canonical-web-and-design/snapcraft.io/issues/1246
+  publisher: "./static/js/publisher/publisher.js",
+};
